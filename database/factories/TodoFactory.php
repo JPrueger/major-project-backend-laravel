9d436279ed9ccc732636518bc7661925961eb4d8
Jeannine Prueger <jeannine.prueger@towa.at>
2022-06-18 19:28:49 +0200

    feat: add TodoFactory

---
+++ database/factories/TodoFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Todo;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Todo>
+ */
+class TodoFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Todo::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->text(20),
+            'description' => $this->faker->text,
+            'category' => $this->faker->randomElement(['Urgent', 'Important', 'For later', 'Blocked', 'Needs focus time']),
+            'is_complete' => $this->faker->boolean,
+        ];
+    }
+}

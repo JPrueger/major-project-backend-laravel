da4c183fb1802c054d8936e7a3fb62ecfbc8881f
Jeannine Prueger <jeannine.prueger@towa.at>
2022-07-26 20:47:48 +0200

    feat: add getDownload() to todo controller

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Todo;
+use Illuminate\Support\Facades\Response;
 
 class TodoController extends Controller
 {
@@ -10,7 +11,21 @@ class TodoController extends Controller
      * Gets all Todos.
      */
     public function getAllTodos() {
-        $todos = Todo::all(['title', 'description', 'category', 'is_complete']);
+        $todos = Todo::all(['id', 'title', 'description', 'category', 'is_complete', 'document']);
         return response()->json($todos);
+    }
+
+    // To-do: get path from DB
+    /**
+     * Gets PDF file.
+     */
+    public function getDownload()
+    {
+        //PDF file is stored under project/public/download/BUWOG.pdf
+        $file= public_path(). "/download/BUWOG.pdf";
+        $headers = array(
+            'Content-Type: application/pdf',
+        );
+        return Response::download($file, 'filename.pdf', $headers);
     }
 }
